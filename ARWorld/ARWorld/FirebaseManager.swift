94f577fa615544ebcf0db71b6c228ec3a2fc6245
Vincent Koo <vkoo@tribalscale.com>
2018-02-03 01:38:46 -0500

    Adding firebase manager class

--- ARWorld/ARWorld/FirebaseManager.swift
+++ ARWorld/ARWorld/FirebaseManager.swift
@@ -13,7 +13,7 @@ import SceneKit
 protocol FirebaseManagerDelegate: class {
     func didAddNode(node: SceneNode)
     func didChangeNode(node: SceneNode)
-
+    func didRemoveNode(id: String)
 }
 
 struct FirebaseManager {
@@ -25,21 +25,100 @@ struct FirebaseManager {
     static let modelsTable_typeColumn = "type"
     static let modelsTable_transformColumn = "transform"
 
-    weak var delegate: FirebaseManagerDelegate?
+    func testDB() {
+        observeOnDelegate(nil)
+        var node = insertNode(type: NodeAssetType.plane, transform: SCNMatrix4Identity)
+        node.transform.m12 = 1234
+        updateNode(node: node)
+        deleteNode(node: node)
+    }
 
-    func testDB(){
-        //set observer
-        observeData(listener: {(snapshot) in
-            if let databaseSnapshot = snapshot.value as? [String : [String : AnyObject]] {
-                FirebaseManager.getSceneNodes(databaseSnapshot: databaseSnapshot)
+    func insertNode(type: NodeAssetType, transform: SCNMatrix4)-> SceneNode {
+        let key = db.child(FirebaseManager.modelsTableName).childByAutoId().key
+
+        db.child(FirebaseManager.modelsTableName)
+            .child(key)
+            .child(FirebaseManager.modelsTable_typeColumn)
+            .setValue(type.rawValue)
+
+        db.child(FirebaseManager.modelsTableName)
+            .child(key)
+            .child(FirebaseManager.modelsTable_transformColumn)
+            .setValue(FirebaseManager.transformToArray(transform: transform))
+
+        return SceneNode(id: key, type: type, transform: transform)
+    }
+
+    func updateNode(node: SceneNode){
+        db.child(FirebaseManager.modelsTableName)
+            .child(node.id)
+            .child(FirebaseManager.modelsTable_typeColumn)
+            .setValue(node.type.rawValue)
+
+        db.child(FirebaseManager.modelsTableName)
+            .child(node.id)
+            .child(FirebaseManager.modelsTable_transformColumn)
+            .setValue(FirebaseManager.transformToArray(transform: node.transform))
+    }
+
+    func deleteNode(node: SceneNode){
+        db.child(FirebaseManager.modelsTableName).child(node.id).removeValue()
+    }
+
+    func observeOnDelegate(_ delegate: FirebaseManagerDelegate?){
+
+        db.child(FirebaseManager.modelsTableName).observe(.childAdded, with: { (snapshot) -> Void in
+            if let nodeData = snapshot.value as? [String : AnyObject] {
+                delegate?.didAddNode(
+                    node: SceneNode(
+                        id: snapshot.key,
+                        type: NodeAssetType.getType(typeName: nodeData[FirebaseManager.modelsTable_typeColumn] as! String),
+                        transformAsArray: nodeData[FirebaseManager.modelsTable_transformColumn] as! [Float]))
             }
         })
 
-        //write test
-        updateNode(type: NodeAssetType.box, transform: SCNMatrix4Identity)
+        db.child(FirebaseManager.modelsTableName).observe(.childChanged, with: { (snapshot) -> Void in
+            if let nodeData = snapshot.value as? [String : AnyObject] {
+                delegate?.didChangeNode(
+                    node: SceneNode(
+                        id: snapshot.key,
+                        type: NodeAssetType.getType(typeName: nodeData[FirebaseManager.modelsTable_typeColumn] as! String),
+                        transformAsArray: nodeData[FirebaseManager.modelsTable_transformColumn] as! [Float]))
+            }
+        })
+
+        db.child(FirebaseManager.modelsTableName).observe(.childRemoved, with: { (snapshot) -> Void in
+            delegate?.didRemoveNode(id: snapshot.key)
+        })
     }
 
-    static func getSceneNodes(databaseSnapshot: [String : [String : AnyObject]]) -> Array<SceneNode>{
+    func getCurrentDatabase(didGetNodes: @escaping ([SceneNode]?) -> Void) {
+        db.child(FirebaseManager.modelsTableName).observeSingleEvent(of: .value, with: {(snapshot) in
+            if let databaseSnapshot = snapshot.value as? [String : [String : AnyObject]] {
+                didGetNodes(FirebaseManager.getSceneNodes(databaseSnapshot: databaseSnapshot) )
+            }
+        })
+    }
+
+    static func transformToArray(transform: SCNMatrix4) -> Array<Float>{
+        //Ready transform as matrix
+        return [
+            transform.m11, transform.m12, transform.m13, transform.m14,
+            transform.m21, transform.m22, transform.m23, transform.m24,
+            transform.m31, transform.m32, transform.m33, transform.m34,
+            transform.m41, transform.m42, transform.m43, transform.m44
+        ]
+    }
+
+    static func arrayToTransform(transformAsArray: Array<Float>) -> SCNMatrix4{
+        //Ready transform as matrix
+        return SCNMatrix4(m11: transformAsArray[0], m12: transformAsArray[1], m13: transformAsArray[2], m14: transformAsArray[3],
+                          m21: transformAsArray[4], m22: transformAsArray[5], m23: transformAsArray[6], m24: transformAsArray[7],
+                          m31: transformAsArray[8], m32: transformAsArray[9], m33: transformAsArray[10], m34: transformAsArray[11],
+                          m41: transformAsArray[12], m42: transformAsArray[13], m43: transformAsArray[14], m44: transformAsArray[15])
+    }
+
+    static func getSceneNodes(databaseSnapshot: [String : [String : AnyObject]]) -> Array<SceneNode> {
         var listOfSceneNodes: Array<SceneNode> = Array()
 
         if let models = databaseSnapshot["models"] {
@@ -53,28 +132,8 @@ struct FirebaseManager {
 
         return listOfSceneNodes
     }
-
-    func updateNode(type: NodeAssetType, transform: SCNMatrix4){
-
-        //Ready transform as matrix
-        let transformAsArray: Array<Float> = [
-            transform.m11, transform.m12, transform.m13, transform.m14,
-            transform.m21, transform.m22, transform.m23, transform.m24,
-            transform.m31, transform.m32, transform.m33, transform.m34,
-            transform.m41, transform.m42, transform.m43, transform.m44
-        ]
-
-        //Ready uuid and write to db
-        let uuid = NSUUID().uuidString
-        db.child(FirebaseManager.modelsTableName).child(uuid).child(FirebaseManager.modelsTable_typeColumn).setValue(type.rawValue)
-        db.child(FirebaseManager.modelsTableName).child(uuid).child(FirebaseManager.modelsTable_transformColumn).setValue(transformAsArray)
-    }
-
-    func observeData(listener : ((DataSnapshot) -> Void)!){
-        db.observe(DataEventType.value, with: listener)
-    }
-
 }
+
 
 struct SceneNode {
     var id: String
@@ -84,10 +143,13 @@ struct SceneNode {
     init(id: String, type: NodeAssetType, transformAsArray: Array<Float>) {
         self.id = id
         self.type = type
-        self.transform = SCNMatrix4(m11: transformAsArray[0], m12: transformAsArray[1], m13: transformAsArray[2], m14: transformAsArray[3],
-                                    m21: transformAsArray[4], m22: transformAsArray[5], m23: transformAsArray[6], m24: transformAsArray[7],
-                                    m31: transformAsArray[8], m32: transformAsArray[9], m33: transformAsArray[10], m34: transformAsArray[11],
-                                    m41: transformAsArray[12], m42: transformAsArray[13], m43: transformAsArray[14], m44: transformAsArray[15])
+        self.transform = FirebaseManager.arrayToTransform(transformAsArray: transformAsArray)
+    }
+
+    init(id: String, type: NodeAssetType, transform: SCNMatrix4) {
+        self.id = id
+        self.type = type
+        self.transform = transform
     }
 }
 
@@ -112,10 +174,10 @@ enum NodeAssetType: String {
 
     static func getType(typeName: String) -> NodeAssetType {
         switch typeName {
-            case NodeAssetType.plane.rawValue:
-                return NodeAssetType.plane
-            default:
-                return NodeAssetType.box
+        case NodeAssetType.plane.rawValue:
+            return NodeAssetType.plane
+        default:
+            return NodeAssetType.box
         }
     }
 }
