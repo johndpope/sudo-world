dcbed937cf6dc05be6275dc647f6b00f57ebaec7
Nate Udren <nudren@tribalscale.com>
2018-02-25 15:17:20 -0500

    Moved files into nice folders

--- ARWorld/Firebase/FirebaseManager.swift
+++ ARWorld/Firebase/FirebaseManager.swift
@@ -137,37 +137,3 @@ struct FirebaseManager {
         return listOfSceneNodes
     }
 }
-
-
-struct FirebaseNode {
-    var id: String
-    var type: NodeAssetType
-    var transform: SCNMatrix4
-
-    init(id: String, type: NodeAssetType, transformAsArray: Array<Float>) {
-        self.id = id
-        self.type = type
-        self.transform = FirebaseManager.arrayToTransform(transformAsArray: transformAsArray)
-    }
-
-    init(id: String, type: NodeAssetType, transform: SCNMatrix4) {
-        self.id = id
-        self.type = type
-        self.transform = transform
-    }
-}
-
-extension FirebaseNode : Equatable {
-    static func ==(lhs: FirebaseNode, rhs: FirebaseNode) -> Bool {
-        if(lhs.id != rhs.id){
-            return false
-        }
-        if(lhs.type == rhs.type){
-            return false
-        }
-        if(SCNMatrix4EqualToMatrix4(lhs.transform, rhs.transform)){
-            return false
-        }
-        return true
-    }
-}
---
+++ ARWorld/Firebase/FirebaseNode.swift
@@ -0,0 +1,42 @@
+//
+//  FirebaseNode.swift
+//  ARWorld
+//
+//  Created by TSD040 on 2018-02-25.
+//  Copyright © 2018 sudo-world. All rights reserved.
+//
+
+import SceneKit
+
+struct FirebaseNode {
+    var id: String
+    var type: NodeAssetType
+    var transform: SCNMatrix4
+    
+    init(id: String, type: NodeAssetType, transformAsArray: Array<Float>) {
+        self.id = id
+        self.type = type
+        self.transform = FirebaseManager.arrayToTransform(transformAsArray: transformAsArray)
+    }
+    
+    init(id: String, type: NodeAssetType, transform: SCNMatrix4) {
+        self.id = id
+        self.type = type
+        self.transform = transform
+    }
+}
+
+extension FirebaseNode : Equatable {
+    static func ==(lhs: FirebaseNode, rhs: FirebaseNode) -> Bool {
+        if(lhs.id != rhs.id){
+            return false
+        }
+        if(lhs.type == rhs.type){
+            return false
+        }
+        if(SCNMatrix4EqualToMatrix4(lhs.transform, rhs.transform)){
+            return false
+        }
+        return true
+    }
+}
